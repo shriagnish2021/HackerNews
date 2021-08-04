b9e40429e70a8cc1dc69cd5a51b26bbe12fb1e5c
nv08 <nyvy.07@gmail.com>
2021-08-04 16:38:17 +0530

    Update winstonLogger.js

--- hackernews/util/winstonLogger.js
+++ hackernews/util/winstonLogger.js
@@ -5,7 +5,7 @@ const logger = createLogger({
   transports: [
     new transports.File({
       name: 'error-file',
-      
+      filename: __dirname + '/error.log'
       level: 'error',
       json: true,
       format: format.combine(
