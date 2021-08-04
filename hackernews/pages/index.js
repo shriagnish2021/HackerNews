f2afac3a13d4d0f48a5203673a8ae2c4c8719e79
Thushar <thushar.work@gmail.com>
2021-08-04 08:43:52 +0530

    Home page

--- hackernews/pages/index.js
+++ hackernews/pages/index.js
@@ -1,9 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import useSWR from 'swr';
 import Header from '../components/Header';
 import ArticleSection from '../components/ArticleSection';
 
 export default function Home() {
+  const { data, error } = useSWR('/api/posts');
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  // const data = fetch('/api/posts');
+  console.log(data);
   return (
     <>
       <Header />
