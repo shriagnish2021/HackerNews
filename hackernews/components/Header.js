0cc486307809b6b21e01ce93b786e3b4a652a1c1
Thushar <thushar.work@gmail.com>
2021-08-05 18:35:28 +0530

    Separate the search component

--- hackernews/components/Header.js
+++ hackernews/components/Header.js
@@ -1,29 +1,36 @@
 import Link from 'next/link';
 import { FaSearch, FaBars, FaEnvelope } from 'react-icons/fa';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Header() {
   const [visibility, setVisibility] = useState(false);
   const searchBarClass = 'flex place-content-center mt-2 ';
+  const Router = useRouter();
+
   return (
     <>
-      <head>
+      {/*  <head>
         <meta charset="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Hacker News</title>
-      </head>
+      </head> */}
       <header className="">
         <div className="bg-blue-800 text-white px-6 py-5 flex place-content-evenly ">
           <h1 className="text-4xl font-black ">
             <Link href="/">The Hacker News</Link>
           </h1>
-          <button type="button" className="bg-yellow-300 text-black p-2 flex rounded-md">
-            <span>
-              <FaEnvelope className="top-0 mt-1 mr-2" />
-            </span>{' '}
-            Subscribe to Newsletter
-          </button>
+          {Router.asPath === '/' ? (
+            <button type="button" className="bg-yellow-300 text-black p-2 flex rounded-md">
+              <span>
+                <FaEnvelope className="top-0 mt-1 mr-2" />
+              </span>{' '}
+              Subscribe to Newsletter
+            </button>
+          ) : (
+            ''
+          )}
         </div>
         <div className="bg-white text-sm px-6 py-3 flex place-content-evenly">
           <section className="space-x-6  ">
@@ -39,7 +46,7 @@ export default function Header() {
             <Link href="/">
               <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">
                 {' '}
-                Vulnerabilities{' '}
+                Login{' '}
               </span>
             </Link>
             <Link href="/">
@@ -54,9 +61,9 @@ export default function Header() {
             </Link>
           </section>
           <section className="space-x-6">
-            <button type="button" onClick={() => setVisibility((prevState) => !prevState)}>
+            {/*  <button type="button" onClick={() => setVisibility((prevState) => !prevState)}>
               <FaSearch />
-            </button>
+            </button> */}
 
             <button type="button">
               <FaBars />
@@ -64,7 +71,17 @@ export default function Header() {
           </section>
         </div>
         <div className={`${searchBarClass} ${visibility ? 'block' : 'hidden'}`}>
-          <input type="text" placeholder="Search Here" className="border-gray-400 border-2 w-7/12 p-1 rounded  " />
+          <input type="text" placeholder="Search Here" className="border-gray-400 border-2  p-1 rounded mr-4 w-1/6 " />
+          <div>
+            <span> Sort by date:&nbsp; </span>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              className="border-gray-400 border-2  p-1 rounded "
+              placeholder="dd/aa/bbbb"
+            />
+          </div>
         </div>
       </header>
     </>
