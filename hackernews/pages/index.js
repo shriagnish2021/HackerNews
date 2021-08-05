77663694829c260d33684cad4abe7583a063fc9e
=nv08 <=>
2021-08-05 10:19:32 +0530

    package.json

--- hackernews/pages/index.js
+++ hackernews/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-   const data = fetch('/api/posts');
+
   //console.log(data);
   return (
     <>
