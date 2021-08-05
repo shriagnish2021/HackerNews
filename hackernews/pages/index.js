97d4dbb2a1bddc74d5d3021f0011da6771c2794d
Thushar <thushar.work@gmail.com>
2021-08-05 10:02:07 +0530

    Add digest panel

--- hackernews/pages/index.js
+++ hackernews/pages/index.js
@@ -1,20 +1,19 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import useSWR from 'swr';
 import Header from '../components/Header';
 import ArticleSection from '../components/ArticleSection';
+import DigestPanel from '../components/DigestPanel';
 
 export default function Home() {
   const { data, error } = useSWR('/api/posts');
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  // const data = fetch('/api/posts');
-  //console.log(data);
+
   return (
     <>
       <Header />
       <ArticleSection />
+      <DigestPanel />
     </>
   );
 }
