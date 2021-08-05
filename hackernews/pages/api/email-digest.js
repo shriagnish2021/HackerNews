0dc33f8cdc3c58b010bd7a3f9dd106768ad61384
DivySharma-mb <divy.sharma.16.1@mountblue.tech>
2021-08-05 10:30:42 +0530

    Changed Email client

--- hackernews/pages/api/email-digest.js
+++ hackernews/pages/api/email-digest.js
@@ -3,16 +3,12 @@ import {
   getSubscribers,
   getTopArticles,
 } from "../../database/queries";
-import mailClient from "nodemailer";
+import sgMail from "@sendgrid/mail";
 
-const client = mailClient.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "d6abca00064884",
-    pass: "d4634532a8fd10"
-  }
-});
+sgMail.setApiKey(
+  process.env.SENDGRID_API_KEY
+);
+
 
 export default async function subscribe(req, res) {
   if (req.method === "POST") {
@@ -35,13 +31,19 @@ export default async function subscribe(req, res) {
     req.headers.authorization === `Bearer ${process.env.EMAIL_PASSWORD}`
   ) {
     try {
+      res.status(200).json({ msg: "Done!" });
       const articles = await getTopArticles();
       const html = createHTML(articles, "Post");
       const subscribers = await getSubscribers();
-      subscribers.forEach((sb) => {
-        sendEmail(sb.email, html);
+      subscribers.forEach(async (sb) => {
+        await sendEmail(sb.email, html)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
       });
-      res.status(200).json({ msg: "Done!" });
     } catch (error) {
       console.log(error);
     }
@@ -50,22 +52,14 @@ export default async function subscribe(req, res) {
   }
 }
 
-function sendEmail(email, data) {
-  client.sendMail(
-    {
-      from: process.env.EMAIL_USERNAME,
-      to: email,
-      subject: "Email Digest - Hacker News!",
-      html: data,
-    },
-    (err, info) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Email Sent: " + info.response);
-      }
-    }
-  );
+async function sendEmail(email, data) {
+  const msg = {
+    to: `${email}`,
+    from: "hacker.news.project@gmail.com",
+    subject: "Hacker News - Email Digest",
+    html: `${data}`,
+  };
+  return sgMail.send(msg);
 }
 
 function createHTML(articleDataArray, type) {
@@ -75,9 +69,9 @@ function createHTML(articleDataArray, type) {
   <h2>Top ${type}s</h2>
   <ol>`;
   articleDataArray.forEach((article) => {
-    emailContentHTML += (`
+    emailContentHTML += `
       <li><a href="https://hacker-news-delta.vercel.app/posts/${article.id}">${article.title}</a></li>
-    `)
+    `;
   });
-  return emailContentHTML + `</ol>`
+  return emailContentHTML + `</ol>`;
 }
