66ebdc6d1e7fc6598e4a1d8eb7a401b5c8901596
Thushar <thushar.work@gmail.com>
2021-08-05 18:36:03 +0530

    Add react toastify for flash messages

--- hackernews/pages/_app.js
+++ hackernews/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
