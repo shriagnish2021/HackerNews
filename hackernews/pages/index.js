5207e3e6a3e49257ba86a2f4e62ad47e7f8337e7
Thushar <thushar.work@gmail.com>
2021-08-05 18:36:31 +0530

    Fetch data in the index page and send it as props

--- hackernews/pages/index.js
+++ hackernews/pages/index.js
@@ -5,14 +5,12 @@ import DigestPanel from '../components/DigestPanel';
 
 export default function Home() {
   const { data, error } = useSWR('/api/posts');
-
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-
   return (
     <>
       <Header />
-      <ArticleSection />
+      <ArticleSection data={data} />
       <DigestPanel />
     </>
   );
