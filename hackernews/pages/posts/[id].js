7c7464c0066a2e1c8d5cda6229460ebff58ff0b6
DivySharma-mb <divy.sharma.16.1@mountblue.tech>
2021-08-06 16:43:05 +0530

    Added comments to post section

--- hackernews/pages/posts/[id].js
+++ hackernews/pages/posts/[id].js
@@ -2,10 +2,16 @@ import useSWR from 'swr';
 import { useRouter } from 'next/router';
 import Header from '../../components/Header';
 import ReadArticle from '../../components/ReadArticle.js';
+import CommentSection from '../../components/CommentSection/CommentSection';
+import {useSession} from 'next-auth';
 
 export default function Article() {
   const Router = useRouter();
   const path = Router.asPath;
+  const {session, loading} = useSession();
+  
+  const articleId = path.split('posts/')[1]
+  const {data: comments, error: err} = useSWR(`/api/comments?id=${articleId}`);
 
   const { data, error } = useSWR(`/api${path}`);
 
@@ -16,6 +22,7 @@ export default function Article() {
     <>
       <Header />
       <ReadArticle article={data} />
+      <CommentSection articleId={articleId} data={comments} session={...session} />
     </>
   );
 }
