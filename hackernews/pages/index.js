282e05bcf55bd6e758ce9594cf9f18d97d98d795
Thushar <thushar.work@gmail.com>
2021-08-06 14:57:48 +0530

    Separate the search component form the header

--- hackernews/pages/index.js
+++ hackernews/pages/index.js
@@ -1,18 +1,25 @@
 import useSWR from 'swr';
+import { useState } from 'react';
 import Header from '../components/Header';
 import ArticleSection from '../components/ArticleSection';
 import DigestPanel from '../components/DigestPanel';
+import Footer from '../components/Footer';
 
 export default function Home() {
+  const [searchBarVisibility, setSearchBarVisibility] = useState({
+    search: false,
+    searchWithinDateRange: false,
+  });
   const { data, error } = useSWR('/api/posts');
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  console.log(data)
+
   return (
     <>
-      <Header />
-      <ArticleSection data={data} />
+      <Header setSearchBarVisibility={setSearchBarVisibility} />
+      <ArticleSection data={data} searchBarVisibility={searchBarVisibility} />
       <DigestPanel />
+      <Footer />
     </>
   );
 }
