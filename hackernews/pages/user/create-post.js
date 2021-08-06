9678f274bcc9d020fc1139e0d4e7bf299d47b198
=nv08 <=>
2021-08-07 05:10:40 +0530

    fix authorID

--- hackernews/pages/user/create-post.js
+++ hackernews/pages/user/create-post.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 import router from 'next/router';
 import Header from '../../components/Header';
 import ArticleInputForm from '../../components/ArticleInputForm';
-import { useState } from 'react';
+import { useSession } from 'next-auth/client';
 
 export default function CreatePost() {
+  const [session] = useSession()
   const handleAddNewArticle = async ({ title, content, file, tags },setLoading) => {
     const formData = new FormData();
     formData.append('title', title);
     formData.append('content', content);
-    formData.append('authorId', 1); //replace with user id from session
+    formData.append('authorId', session.user.id); //replace with user id from session
     formData.append('img', file);
     formData.append('tags', tags)
     
