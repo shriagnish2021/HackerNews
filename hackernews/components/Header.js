a9a48025598a1f7ec0a9c692bd83a036da1d8074
Thushar R P <thushar.work@gmail.com>
2021-08-06 16:15:15 +0530

    Update header after conflict

--- hackernews/components/Header.js
+++ hackernews/components/Header.js
@@ -1,104 +1,97 @@
-
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable @next/next/link-passhref */
-import Link from "next/link";
-import { FaSearch, FaBars, FaEnvelope, FaUser } from "react-icons/fa";
-import { useState } from "react";
-import Login from "../pages/login";
-import { signOut, useSession } from "next-auth/client";
+import Link from 'next/link';
+import { FaSearch, FaBars, FaEnvelope, FaUser, FaFilter } from 'react-icons/fa';
+import { useState } from 'react';
+import { signOut, useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Login from '../pages/login';
 
 export default function Header({ setSearchBarVisibility }) {
   const [visibility, setVisibility] = useState(false);
   const [login, setlogin] = useState(false);
   const [session, loading] = useSession();
-  const searchBarClass = "flex place-content-center mt-2 ";
+  const searchBarClass = 'flex place-content-center mt-2 ';
   const Router = useRouter();
-
 
   function handleScroll() {
     window.scroll({
       top: document.body.offsetHeight,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   }
   return (
     <>
-       <Head>
+      <Head>
         <meta charset="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Hacker News</title>
         <link rel="icon" href="/images/logo.png" type="image/icon type" />
       </Head>
-      
-    <header className="">
-      <div className="bg-blue-800 text-white px-6 py-5 flex place-content-evenly ">
-        <h1 className="text-4xl font-black ">
-          <Link href="/">The Hacker News</Link>
-        </h1>
 
-        {Router.asPath === '/' ? (
-            <button type="button" className="bg-yellow-300 text-black p-2 flex rounded-md"
-          onClick={()=>handleScroll()} >
+      <header className="">
+        <div className="bg-blue-800 text-white px-6 py-5 flex place-content-evenly ">
+          <h1 className="text-4xl font-black ">
+            <Link href="/">The Hacker News</Link>
+          </h1>
+
+          {Router.asPath === '/' ? (
+            <button
+              type="button"
+              className="bg-yellow-300 text-black p-2 flex rounded-md"
+              onClick={() => handleScroll()}
+            >
               <span>
                 <FaEnvelope className="top-0 mt-1 mr-2" />
               </span>{' '}
-              Subscribe 
+              Subscribe
             </button>
           ) : (
             ''
           )}
-      <div>
+          <div>
+            {login ? <Login setlogin={setlogin} /> : null}
 
-      
-        {login ? <Login setlogin={setlogin} /> : null}
-        
-        {session && !loading ? (
-          <>
-         
-          <span>{session.user.image?<img src={session.user.image} alt="user-png" className="h-5" />:<FaUser /> }{session.user.userName}</span>
-          <button onClick={() => signOut()} className="ml-4" > Logout</button>
-        </>
-         
-        ) : (
-          <button onClick={() => setlogin(true)}>Login</button>
-        )}
+            {session && !loading ? (
+              <>
+                <span>
+                  {session.user.image ? <img src={session.user.image} alt="user-png" className="h-5" /> : <FaUser />}
+                  {session.user.userName}
+                </span>
+                <button type="button" onClick={() => signOut()} className="ml-4">
+                  {' '}
+                  Logout
+                </button>
+              </>
+            ) : (
+              <button type="button" onClick={() => setlogin(true)}>
+                Login
+              </button>
+            )}
+          </div>
         </div>
-          
-      </div>
 
-      <div className="bg-white text-sm px-6 py-3 flex place-content-evenly">
-        <section className="space-x-6  ">
-          <Link href="/">
-            <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">
-              Home
-            </span>
-          </Link>
-          {session ? (
-            
+        <div className="bg-white text-sm px-6 py-3 flex place-content-evenly">
+          <section className="space-x-6  ">
+            <Link href="/">
+              <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">Home</span>
+            </Link>
+            {session ? (
               <Link href="/user/create-post">
                 <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">
                   New Article
                 </span>
               </Link>
-             
-           
-          ) : null}
-          <Link href="/postJob">
-            <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">
-              Post Job
-            </span>
-          </Link>
-         
-          <Link href="/Jobs">
-                <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">
-                  Jobs
-                </span>
-              </Link>
-        </section>
-        {Router.asPath === '/' ? (
+            ) : null}
+            <Link href="/postJob">
+              <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">Post Job</span>
+            </Link>
+
+            <Link href="/Jobs">
+              <span className=" border-transparent  border-b-4 cursor-pointer hover:border-blue-800 p-1">Jobs</span>
+            </Link>
+          </section>
+          {Router.asPath === '/' ? (
             <section className="space-x-6">
               <button
                 type="button"
@@ -141,7 +134,9 @@ export default function Header({ setSearchBarVisibility }) {
           ) : (
             ''
           )}
-    </header>
-</>
+        </div>
+      </header>
+    </>
   );
 }
+
